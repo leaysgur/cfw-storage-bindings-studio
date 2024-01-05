af04f944c8170487df655c1ac96990257f47767f
Yuji Sugiura <y.sugiura.0316@gmail.com>
2024-01-05 14:03:39 +0900

    Use runes

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
+  compilerOptions: {
+    runes: true,
+  },
   kit: {
     adapter: adapter({ fallback: "index.html" }),
     prerender: { entries: [] },
